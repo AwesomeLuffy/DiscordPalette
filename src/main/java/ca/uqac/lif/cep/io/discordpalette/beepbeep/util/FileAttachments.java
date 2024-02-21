b8bdd11d450bfebc4f649da422bd3a59c2c40621
Yolan <yrenard@et.esiea.fr>
2024-02-21 15:09:27 -0500

    Adding FileAttachments utility class

---
+++ src/main/java/ca/uqac/lif/cep/io/discordpalette/beepbeep/util/FileAttachments.java
@@ -0,0 +1,72 @@
+package ca.uqac.lif.cep.io.discordpalette.beepbeep.util;
+
+import ca.uqac.lif.cep.functions.UnaryFunction;
+import net.dv8tion.jda.api.entities.Message;
+
+public class FileAttachments {
+    public static final FileName fileName = new FileName();
+    public static final FormatedFileName formatedFileName = new FormatedFileName();
+    public static final FileExtension fileExtension = new FileExtension();
+
+    protected FileAttachments(){}
+
+    /**
+     * Get the full file name
+     */
+    public static final class FileName extends UnaryFunction<Message.Attachment, String> {
+
+        private FileName() {
+            super(Message.Attachment.class, String.class);
+        }
+
+        @Override
+        public String getValue(Message.Attachment attachment) {
+            return attachment.getFileName();
+        }
+
+        @Override
+        public String toString() {
+            return "FileName";
+        }
+    }
+
+    /**
+     * Get only the name of the file without the extensions
+     */
+    public static final class FormatedFileName extends UnaryFunction<Message.Attachment, String>{
+
+        private FormatedFileName() {
+            super(Message.Attachment.class, String.class);
+        }
+
+        @Override
+        public String getValue(Message.Attachment attachment) {
+            return attachment.getFileName().substring(0, attachment.getFileName().lastIndexOf('.'));
+        }
+
+        @Override
+        public String toString() {
+            return "FormatedFileName";
+        }
+    }
+
+    /**
+     * Get the extension of the file
+     */
+    public static final class FileExtension extends UnaryFunction<Message.Attachment, String> {
+
+        private FileExtension() {
+            super(Message.Attachment.class, String.class);
+        }
+
+        @Override
+        public String getValue(Message.Attachment attachment) {
+            return attachment.getFileExtension();
+        }
+
+        @Override
+        public String toString() {
+            return "FileExtension";
+        }
+    }
+}
